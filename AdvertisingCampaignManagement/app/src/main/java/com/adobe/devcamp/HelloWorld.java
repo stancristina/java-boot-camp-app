ca6c48114f5e9bbdc16a16092d2e4a9b58c88a42
Vlad Coteanu <vlad.coteanu@uipath.com>
2019-10-23 20:21:37 +0300

    Created Hello World

--- AdvertisingCampaignManagement/app/src/main/java/com/adobe/devcamp/HelloWorld.java
+++ AdvertisingCampaignManagement/app/src/main/java/com/adobe/devcamp/HelloWorld.java
@@ -1,8 +1,17 @@
 package com.adobe.devcamp;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/javadevcamp",
+                "jdc",
+                "devcamp" );
         System.out.println("Hello World");
     }
 
